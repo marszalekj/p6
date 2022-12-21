d2ff47c688d3aff59fc07af20318674a6d0a297f
Jeremy <marszalekbcn@gmail.com>
2022-12-21 09:38:47 +0900

    commentaires

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// import pour pouvoir recevoir et repondre a des requetes http
 const http = require("http");
+// import de l'application
 const app = require("./app");
-
+// port a utiliser par le serveur
 app.set("port", process.env.PORT);
 const server = http.createServer(app);
-
+// methode qui ecoute les requetes envoyees au serveur sur le port indique
 server.listen(process.env.PORT);
